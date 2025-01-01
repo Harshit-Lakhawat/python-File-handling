20c6b1f4f6ef04861778d50eca07b3db99313af3
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-01 16:32:33 +0530

    Create GUI by tkinter.py

---
+++ GUI BY TKINTER.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("BRO NANANINARE NANINENARO")
+window.config(background = "black")
+window.mainloop()  # place window on screen
