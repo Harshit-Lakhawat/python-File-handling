aef6c16ef12813f7519a0328c0f067d187237f2b
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-03 15:53:07 +0530

    Update and rename menubar.py to GUI menubar and frame.py

--- GUI menubar and frame.py
+++ GUI menubar and frame.py
@@ -20,6 +20,28 @@ def paste():
 
 window = Tk()
 
+
+
+
+# frame
+
+from tkinter import *
+from tkinter import filedialog
+
+window = Tk()
+
+frame = Frame(window, bg = "light yellow",bd=5,relief=SUNKEN)
+#frame.pack(side = BOTTOM)     #place frame at bottom
+frame.place(x=0,y=0)           #place frame at cooredinates
+
+button = Button(frame,text="W",font=("Consolas",25),width = 3)
+button.pack(side=TOP)
+Button(frame,text="A",font=("Consolas",25),width = 3).pack(side =LEFT)
+Button(frame,text="S",font=("Consolas",25),width = 3).pack(side =LEFT)
+Button(frame,text="D",font=("Consolas",25),width = 3).pack(side =LEFT)
+
+window.mainloop()
+
 menubar = Menu(window)
 window.config(menu=menubar)
 
