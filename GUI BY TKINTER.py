1364df790fb5076c02cd25340caa8f0496655f27
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-04 19:18:06 +0530

    Update GUI BY TKINTER , label photo and text.py

--- GUI BY TKINTER.py
+++ GUI BY TKINTER.py
@@ -6,17 +6,26 @@ window.title("BRO NANANINARE NANINENARO")
 window.config(background = "black")
 window.mainloop()  # place window on screen
 
+
+
 # for i#age and text in the window
 
 from tkinter import *
+from PIL import Image, ImageTk
+
 
 window = Tk()
 window.geometry("650x650")
 window.title("BRO NANANINARE NANINENARO")
 window.config(background = "black")
 
+# Load and resize the image
+original_image = Image.open('picture.png')
+desired_width = 200  # set the width
+desired_height = 100  # set the height
+resized_image = original_image.resize((desired_width,desired_height),Image.Resampling.LANCZOS)  #use so that quality of i#age is good Image.Resampling.LANCZOS
+photo = ImageTk.PhotoImage(resized_image)
 
-photo = PhotoImage(file = 'picture.png')
 label = Label(window,text = "sabaash hackerrrr!!",  #for adding text
               font=('Arial',20,'bold'),   #for font,size and type
               fg ="red",                  #for font colour
@@ -26,12 +35,9 @@ label = Label(window,text = "sabaash hackerrrr!!",  #for adding text
               padx = 20,                  #for distance from x-axis of border
               pady = 20,                  #for distance from y-axis of border
               image = photo,              #for photo
-              compound = 'top'         #so that it know where to put photo regards to text
+              compound = 'bottom'         #so that it know where to put photo regards to text
               )
 # label.place(x=0,y=1)
 label.pack()
 
 window.mainloop()  # place window on screen
-
-
-
