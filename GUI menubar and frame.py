1b33c66a4dba0dafbcfbf7d4a0b2733c0dcce695
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-05 16:22:42 +0530

    Update GUI menubar and frame.py

--- GUI menubar and frame.py
+++ GUI menubar and frame.py
@@ -63,22 +63,4 @@ Button(frame,text="D",font=("Consolas",25),width = 3).pack(side =LEFT)
 
 window.mainloop()
 
-menubar = Menu(window)
-window.config(menu=menubar)
 
-filemenu = Menu(menubar, tearoff=0 ) #to get rid of line on top tearoff=0
-menubar.add_cascade(label="file", menu =filemenu)
-filemenu.add_command(label="open",command=openfile)
-filemenu.add_command(label="save",command=savefile)
-filemenu.add_separator()
-filemenu.add_command(label="exit",command=quit)
-
-
-editmenu = Menu(menubar, tearoff=0 )
-menubar.add_cascade(label="edit", menu=editmenu)
-editmenu.add_command(label="cut",command=cut)
-editmenu.add_command(label="save",command=copy)
-editmenu.add_separator()
-editmenu.add_command(label="paste",command=paste)
-
-window.mainloop()
