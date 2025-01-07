4b60bc1f067b8beb9f7e5c42205a7aff3fc4e735
Harshit Lakhawat <110206harshit@gmail.com>
2025-01-07 13:46:16 +0530

    Update and rename GUI drag and drop widget.py to GUI drag and drop widget and move widget within window.py

--- GUI drag and drop widget and move widget within window.py
+++ GUI drag and drop widget and move widget within window.py
@@ -28,3 +28,28 @@ label2.bind("<Button-1>", drag_start)
 label2.bind("<B1-Motion>", drag_motion)
 
 window.mainloop()
+
+
+
+
+
+# move widget within window
+
+from tkinter import *
+from PIL import Image, ImageTk
+
+window = Tk()
+window.geometry("500x500")
+
+racecar_image = Image.open('racecar.png')
+desired_width = 250# set the width
+desired_height = 150  # set the height
+resized_image = racecar_image.resize((desired_width,desired_height),Image.Resampling.LANCZOS)  #use so that quality of i#age is good Image.Resampling.LANCZOS
+racecarimage = ImageTk.PhotoImage(resized_image)
+
+label = Label(window,image=racecarimage)
+label.place(x=0,y=0)
+
+window.mainloop()
+
+
